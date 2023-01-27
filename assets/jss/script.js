969b48a23b89d67d0a5dc2129a0f644f9675ed6a
Skwiera <mckskwiera@gmail.com>
2023-01-27 18:48:02 +0000

    weather for today shows correctly after changing api link

--- assets/jss/script.js
+++ assets/jss/script.js
@@ -1,6 +1,13 @@
 let log = console.log.bind(document)
 let apiKey = '9ad056eba8f6a8e60e24aa47ad3f2ea9'
 
+let tomorrow  = moment().add(1,'days').format('DD/MM/YYYY');
+console.log(tomorrow)
+
+
+// $('.form-inline').attr('id', 'form-inline')
+// $('#form-inline').removeAttr('class')
+// $('#form-inline').attr('class', 'form')
 // setting up event listener on search button
 $('.search-button').on('click', function (event) {
   // preventing text from the form to be pushed to end of the link
@@ -9,24 +16,56 @@ $('.search-button').on('click', function (event) {
   let city = $("#search-input").val().trim();
   let geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&units=metric&appid=${apiKey}`
 
+
   $.ajax({
     url: geoURL,
     method: "GET"
   }).then(function (response) {
+    //log(response)
     let lat = response[0].lat
     let lon = response[0].lon
-    let queryURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
+    let queryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&cnt=6`
     $.ajax({
       url: queryURL,
       method: "GET"
-    }).then(function (weatherResponse) {
+    }).then(function renderForecast(weatherResponse) {
       log(weatherResponse)
+      log(weatherResponse.list[0].main.feels_like)
       $('#today').empty()
       $('#today').append(`
-      <h3>${weatherResponse.name} ${moment().format('DD/MM/YYYY')}</h3>
-      <p>Temperature: ${(weatherResponse.main.temp - 273.15).toFixed(2)}C Degrees</p>
-      <p>Wind speed: ${weatherResponse.wind.speed}km/h</p>
-      <p>Humidity: ${weatherResponse.main.humidity}%</p>
+      <div class="border p-2">
+      <h3>${weatherResponse.city.name} ${moment().format('DD/MM/YYYY')}</h3>
+      <p>Temperature: ${(weatherResponse.list[0].main.feels_like - 273.15).toFixed(2)}C Degrees</p>
+      <p>Wind speed: ${weatherResponse.list[0].wind.speed}km/h</p>
+      <p>Humidity: ${weatherResponse.list[0].main.humidity}%</p>
+      </div>
+      `)
+      $('#history').prepend(`
+      <button class="btn btn-secondary mb-2">${weatherResponse.city.name}</button>
+      `)
+      // pushing forecast to the site after removing previous results
+      $('#forecast').empty()
+      $('#forecast').append(`
+      <div class="container">
+      <div class="row">
+      <h3>5-Day Forecast:</h3>
+      <div class="col-sm forecast-1 border">
+      1 of 5 columns
+      </div>
+      <div class="col-sm forecast-2 border">
+      2 of 5 columns
+      </div>
+      <div class="col-sm forecast-3 border">
+      3 of 5 columns
+      </div>
+      <div class="col-sm forecast-4 border">
+      4 of 5 columns
+      </div>
+      <div class="col-sm forecast-5 border">
+      5 of 5 columns
+      </div>
+      </div>
+      </div>
       `)
     })
   });
